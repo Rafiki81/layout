b11bd06fc192bcbd01ee6d7e8edd38ecdd63c4e3
Rafael Perez-Beato <rperezbeato@gmail.com>
2020-05-24 20:37:58 +0200

    Se añade el layout de land.

--- app/src/main/java/com/ilerna/layout/MainActivity.java
+++ app/src/main/java/com/ilerna/layout/MainActivity.java
@@ -3,12 +3,42 @@ package com.ilerna.layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonRegister;
+    Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonRegister = findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                functionRegister(v);
+            }
+        });
+
+        buttonLogin = findViewById(R.id.buttonlogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                funcionLogin(v);
+            }
+        });
+    }
+
+    private void functionRegister(View v) {
+
+        Toast.makeText(this, getString(R.string.message_register), Toast.LENGTH_SHORT).show();
+    }
+
+    public void funcionLogin(View v){
+
+        Toast.makeText(this, getString(R.string.message_login), Toast.LENGTH_SHORT).show();
     }
 }
