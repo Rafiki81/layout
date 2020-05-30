4622baccf6d6bcd8cceda7707540afd3d6637a38
Rafiki <rperezbeato@gmail.com>
2020-05-30 21:59:37 +0200

    Creacion de los listener en los botones.

--- app/src/main/java/com/ilerna/layout/SoundActivity.java
+++ app/src/main/java/com/ilerna/layout/SoundActivity.java
@@ -5,13 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SoundActivity extends AppCompatActivity {
+    Button ladrar;
+    Button parar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+        ladrar = findViewById(R.id.ladrar);
+        parar = findViewById(R.id.parar);
+
+        ladrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ladrar(v);
+            }
+        });
+        parar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parar(v);
+            }
+        });
     }
 
     public void ladrar(View view){
--- app/src/main/java/com/ilerna/layout/SoundService.java
+++ app/src/main/java/com/ilerna/layout/SoundService.java
@@ -2,12 +2,14 @@ package com.ilerna.layout;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class SoundService extends Service {
+    private MediaPlayer player;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,12 +19,21 @@ public class SoundService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this,"El Sonido se ha iniciado", Toast.LENGTH_SHORT).show();
+        if(player !=null && player.isPlaying()){
+            player.stop();
+        }
+        player = MediaPlayer.create(this,R.raw.ladridos);
+        player.setLooping(true);
+        player.start();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy(){
         Toast.makeText(this,"El Sonido se ha parado", Toast.LENGTH_SHORT).show();
+        if(player!=null){
+            player.release();
+        }
         super.onDestroy();
     }
 }
